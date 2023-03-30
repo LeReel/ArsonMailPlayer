9db6f9819867208f8f44cb996375e41f458aa1fb
LeShrexpert <guyonnardrobin@gmail.com>
2023-03-30 22:05:39 +0200

    Commented libcurl calls

--- Source/MailAPIsManager.cpp
+++ Source/MailAPIsManager.cpp
@@ -10,7 +10,7 @@
 
 #include "MailAPIsManager.h"
 
-#include <include/curl/curl.h>
+//#include <include/curl/curl.h>
 
 struct MemoryStruct
 {
@@ -88,71 +88,70 @@ MailAPIsManager::~MailAPIsManager()
 
 void MailAPIsManager::OpenGmail(char* _username, char* _password)
 {
-    CURL* curl;
-    CURLcode res;
-
-    curl = curl_easy_init();
-    if (curl)
-    {
-        username = _username;
-        password = _password;
-        struct MemoryStruct chunk;
-
-        chunk.memory = (char*)malloc(1); //crecerá según sea necesario con el realloc
-        chunk.size = 0; //no hay datos en este punto
-
-        //inicializacion
-        curl_global_init(CURL_GLOBAL_ALL);
-        curl = curl_easy_init();
-
-
-        //login
-        curl_easy_setopt(curl, CURLOPT_USERNAME, _username);
-        curl_easy_setopt(curl, CURLOPT_PASSWORD, _password);
-
-        popsAccount = "pop3s://pop.gmail.com:995/"; //Adding '1' retrieves attachments
-
-        curl_easy_setopt(curl, CURLOPT_URL, popsAccount);
-        curl_easy_setopt(curl, CURLOPT_USE_SSL, CURLUSESSL_ALL);
-        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0);
-
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, this->WriteMemoryCallback);
-        curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
-
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void*)&chunk);
-
-        //some servers needs this validation
-        curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcurl-agent/1.0");
-
-        /* Perform the request, res will get the return code */
-        res = curl_easy_perform(curl);
-
-        /* Check for errors */
-        if (res != CURLE_OK)
-        {
-            fprintf(stderr, "curl_easy_perform() failed: %s\n",
-                    curl_easy_strerror(res));
-        }
-        else
-        {
-            /*
-            here is where you can work with the data inside the chunk...
-            */
-            printf("%s\n", chunk.memory); //here is the information
-            printf("%lu bytes retrieved\n", (long)chunk.size);
-        }
-
-        /* always cleanup */
-        curl_easy_cleanup(curl);
-
-        //se libera la memoria si hay datos
-        if (chunk.memory)
-            free(chunk.memory);
-        /* always cleanup */
-
-        curl_global_cleanup();
-    }
+    // CURL* curl;
+    // CURLcode res;
+    //
+    // curl = curl_easy_init();
+    // if (curl)
+    // {
+    //     username = _username;
+    //     password = _password;
+    //     struct MemoryStruct chunk;
+    //
+    //     chunk.memory = (char*)malloc(1);
+    //     chunk.size = 0;
+    //
+    //     //initialisation
+    //     curl_global_init(CURL_GLOBAL_ALL);
+    //     curl = curl_easy_init();
+    //
+    //
+    //     //login
+    //     curl_easy_setopt(curl, CURLOPT_USERNAME, _username);
+    //     curl_easy_setopt(curl, CURLOPT_PASSWORD, _password);
+    //
+    //     popsAccount = "pop3s://pop.gmail.com:995/"; //Adding '1' retrieves attachments
+    //
+    //     curl_easy_setopt(curl, CURLOPT_URL, popsAccount);
+    //     curl_easy_setopt(curl, CURLOPT_USE_SSL, CURLUSESSL_ALL);
+    //     curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
+    //     curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0);
+    //
+    //     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, this->WriteMemoryCallback);
+    //     curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
+    //
+    //     curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void*)&chunk);
+    //
+    //     //some servers needs this validation
+    //     curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+    //
+    //     /* Perform the request, res will get the return code */
+    //     res = curl_easy_perform(curl);
+    //
+    //     /* Check for errors */
+    //     if (res != CURLE_OK)
+    //     {
+    //         fprintf(stderr, "curl_easy_perform() failed: %s\n",
+    //                 curl_easy_strerror(res));
+    //     }
+    //     else
+    //     {
+    //         /*
+    //         here is where you can work with the data inside the chunk
+    //         */
+    //         printf("%s\n", chunk.memory); //here is the information
+    //         printf("%lu bytes retrieved\n", (long)chunk.size);
+    //     }
+    //
+    //     /* always cleanup */
+    //     curl_easy_cleanup(curl);
+    //
+    //     if (chunk.memory)
+    //         free(chunk.memory);
+    //     /* always cleanup */
+    //
+    //     curl_global_cleanup();
+    // }
 }
 
 size_t MailAPIsManager::WriteMemoryCallback(char* contents, size_t size, size_t nmemb, void* userp)
